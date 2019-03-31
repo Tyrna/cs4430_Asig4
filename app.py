c0f32b505267d0368a37bdc39a3b80b1f8bfb787
Oscar <oscar.vanderhorstvalette@wmich.edu>
2019-03-30 21:30:57 -0400

    Option 1 and 3

--- app.py
+++ app.py
@@ -24,19 +24,70 @@ def checkNum(num):
 #Option selects
 def switch_options(number, mydb):
     if (number == 1):
-        return 0
+        print("\n\nPlease provide the ID of the customer you wish to add: ")
+        ID = input()
+        if (not ID or len(ID) > 5):
+            print("\nInvalid Input. Please provide a proper CustomerID (Max. 5 characters)")
+            return 0
+        if (database_connect.getCustomerInfo(mydb, ID)):
+            info = get_customer_info()
+            if (info):
+                if (not database_connect.addCustomer(mydb, ID, info)):
+                    database_connect.getCustomerInfo(mydb, ID)
+                    print("\nCustomer added to 'customers'.")
+                    return 0
+        else:
+            print("\n\nA customer with this ID already exists, info above")
+                
+        return 1
     elif (number == 2):
         return 0
     elif (number == 3):
+        print("\n\nPlease provide the ID of the order you wish to remove: ")
+        ID = input()
+        ret = database_connect.getOrderInfo(mydb, ID)
+        if(not ret or ret == 2):
+            print("\nAre you sure you wish to remove the above order? Y/N")
+            confirm = input()
+            if (confirm == 'Y' or confirm == 'y'):
+                if (not database_connect.removeOrder(mydb, ID)):
+                    database_connect.getOrderInfo(mydb, ID)
+                    print("\nOrder removed from 'orders' and 'order_details'.")
+                    return 0
+        else:
+            print("\n\n------- No order with given ID was found. -------\n\n")
+
         return 0
     elif (number == 4):
+        #Id of the Order
+        print("\n\nPlease provide the ID of the order you wish to ship: ")
+        ID = input()
+        ret = database_connect.getOrderInfo(mydb, ID)
+        if (ret == 2):
+            print("\n\nOrder has already been shipped!")
+            return 0
+
+        if (not ret):
+            print("\nDo you wish to ship the above order today? Y/N")
+            confirm = input()
+            if (confirm == 'Y' or confirm == 'y'):
+                if (not database_connect.shipOrder(mydb, ID)):
+                    database_connect.getOrderInfo(mydb, ID)
+                    print("\nAbove order has been shipped.")
+                    return 0
+            
+            print("------- Order has not been shipped. -------")    
+
+        else:
+            print("\n\n------- No order with given ID was found. -------\n\n")
+
         return 0
     elif (number == 5):
         database_connect.pending(mydb)
         return 0
 
     elif (number == 6):
-        print("\n\nPlease provide the ID or the Name of the product you wish to restock: ")
+        print("\n\nPlease provide the ID of the product you wish to restock: ")
         ID = input()
         if (not database_connect.getProductInfo(mydb, ID)):
             print("\nHow much do you wish to add to the product above? (Between 0 - 100)")
@@ -44,13 +95,16 @@ def switch_options(number, mydb):
             if (checkNum(toAdd)):
                 return
 
-            #This is to just add some at time
+            #This is to just limit how many to add at a time
             if (int(toAdd) < 0 or int(toAdd) > 100):
                 print("\nPlease provide a valid number between 0 - 100")
                 return
 
             database_connect.restock(mydb, ID, toAdd)
             database_connect.getProductInfo(mydb, ID)
+        else:
+            print("\n\n------- No product with given ID was found. -------\n\n")
+
         return 0
 
     elif (number == 7):
@@ -61,9 +115,96 @@ def switch_options(number, mydb):
         return 1
         
 
+#Obtains all the information needed to create a customer 
+def get_customer_info():
+    allInfo = []
+
+    print("\nWhat is the 'Company Name'?")
+    info = input()
+    if (info and len(info)<40):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper company name (Max. 40 characters)") 
+        return 0
+
+    print("\nWhat is the 'Contact Name'?")
+    info = input()
+    if (info and len(info)<30):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper contact name (Max. 30 characters)") 
+        return 0
+
+    print("\nWhat is the 'Contact Title'?")
+    info = input()
+    if (info and len(info)<30):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper contact title (Max. 30 characters)") 
+        return 0
+
+    print("\nWhat is the 'Address'?")
+    info = input()
+    if (info and len(info)<30):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper address (Max. 60 characters)") 
+        return 0
+
+    print("\nWhat is the 'City'?")
+    info = input()
+    if (info and len(info)<15):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper city (Max. 15 characters)") 
+        return 0
+
+    print("\nWhat is the 'Region'?")
+    info = input()
+    if (info and len(info)<15):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper region (Max. 15 characters)") 
+        return 0
+
+    print("\nWhat is the 'Postal Code'?")
+    info = input()
+    if (info and len(info)<10):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper postal code (Max. 10 characters)") 
+        return 0
+
+    print("\nWhat is the 'Country'?")
+    info = input()
+    if (info and len(info)<15):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper country (Max. 15 characters)") 
+        return 0 
+
+    print("\nWhat is the 'Phone Number'?")
+    info = input()
+    if (info and len(info)<24):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper phone number (Max. 24 characters)") 
+        return 0
+
+    print("\nWhat is the 'Fax'?")
+    info = input()
+    if (info and len(info)<24):
+        allInfo.append(info) 
+    else:
+        print("Invalid Input. Give a proper fax number (Max. 24 characters)") 
+        return 0
+        
+    return allInfo
+
 #Prints menu and gets user input
 def menu():
-    print("\n1. add customer"   \
+    print("-" * 60  \
+    + "\n1. add customer"   \
     + "\n2. add an order"       \
     + "\n3. remover an order"   \
     + "\n4. ship an order"      \
--- modules/database_connect.py
+++ modules/database_connect.py
@@ -13,6 +13,25 @@ def connectDb():
 
     return mydb
 
+#Gets information from a custormer
+def getCustomerInfo(db, ID):
+    cursor = db.cursor()
+
+    #Prepare SQL -- Get Info of a customer given an ID
+    sql = "SELECT CustomerID, CompanyName, ContactName, ContactTitle, Address, Country, Phone from customers WHERE CustomerID = %s"
+    val = (ID, )
+
+    #Get information and present it to the user
+    cursor.execute(sql, val)
+    result = cursor.fetchall()
+
+    if (not result):
+        return 1
+    else:
+        table_print.printTable(result, ["CustomerID", "CompanyName", "ContactName", "ContactTitle", "Address", "Country", "Phone"])
+        return 0
+
+
 #Gets information from a product
 def getProductInfo(db, ID):
     cursor = db.cursor()
@@ -21,16 +40,120 @@ def getProductInfo(db, ID):
     sql = "SELECT ProductID, ProductName, UnitsInStock from products WHERE ProductId = %s"
     val = (ID, )
 
+    #Get information and present it to the user
     cursor.execute(sql, val)
-
-    #Prepare presentation to the user
     result = cursor.fetchall()
+
     if (not result):
-        print("\n\n------- No product with given ID was found. --------\n\n")
         return 1
     else:
         table_print.printTable(result, ["ProductID", "ProductName", "UnitsInStock"])
         return 0
+
+#Gets information from an order
+def getOrderInfo(db, ID):
+    cursor = db.cursor()
+
+    #Prepare SQL -- Get Info of order given an ID
+    sql = "SELECT OrderID, CustomerID, OrderDate, ShippedDate FROM orders WHERE OrderID = %s"
+    val = (ID, )
+
+    #Get information and present it to the user
+    cursor.execute(sql, val)
+    result = cursor.fetchall()
+
+    if (not result):
+        return 1
+    else:
+        table_print.printTable(result, ["OrderID", "CustomerID", "OrderDate", "ShippedDate"])
+        # If it hasn't been shipped yet...
+        if(not result[0][3]):
+            return 0
+        return 2
+
+#1th Option. Inserts a new customer into 'customers'
+def addCustomer(db, ID, info):
+    cursor = db.cursor()
+
+    #Prepare SQL -- Inserts a new customer into the table 'customers'
+    sql = "INSERT INTO customers (CustomerID, CompanyName, ContactName, ContactTitle, Address, City," \
+        "Region, PostalCode, Country, Phone, Fax) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (ID, info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8], info[9])
+
+    #Execute and commit to the dabatase
+    cursor.execute(sql, val)
+    db.commit()
+
+    #Prepare presentation to the user
+    print("\n\nDB:", cursor.rowcount, "record(s) inserted")
+    print("DB: Successfully added customer into customers")
+
+    return 0
+
+#3th Option. Removes an order from orders & order_details. Updates 'UnitsOnOrder'
+def removeOrder(db, ID):
+    cursor = db.cursor()
+
+    #Prepare SQL -- Delete ALL order_details from order_details that have ID
+    sql = "DELETE FROM order_details WHERE OrderID = %s"
+    val = (ID, )
+
+    cursor.execute(sql, val)
+    
+    #Preate SQL -- Delete order ID from orders
+    sql = "DELETE FROM orders WHERE OrderID = %s"
+    val = (ID, )
+
+    cursor.execute(sql, val)
+
+    #Prepare SQL -- Update 'UnitsOnOrder' from products?
+
+    #Commit to the database
+    db.commit()
+
+    #Prepare presentation to the user
+    print("\n\nDB:", cursor.rowcount, "record(s) updated")
+    print("DB: Successfully removed order from orders and order_details")
+
+    return 0
+
+#4th Option. Ships an order that hasn't been shipped
+
+def shipOrder(db, ID):
+    cursor = db.cursor()
+
+    #Prepare SQL -- Updates 'ShippedDate' from 'NULL' to Current Date
+    sql = "UPDATE orders SET ShippedDate = CURRENT_TIMESTAMP() WHERE OrderID = %s"
+    val = (ID, )
+
+    cursor.execute(sql, val)
+
+    #Commit to the database
+    db.commit()
+
+    #Prepare presentation to the user
+    print("\n\nDB:", cursor.rowcount, "record(s) updated")
+    print("DB: Successfully updated ShippedDate for the order:")
+
+    return 0
+
+#5th Option. Gets pending orders
+def pending(db):
+    cursor = db.cursor()
+
+    #Prepare SQL -- Get Info of order without a shipped date
+    sql = "SELECT DISTINCT OrderID, OrderDate, CompanyName, ContactName, Country, Phone" \
+     + " FROM customers NATURAL JOIN (SELECT OrderID, CustomerID, OrderDate" \
+     + " FROM orders WHERE ShippedDate IS NULL) R1";
+
+    cursor.execute(sql)
+    
+    #Prepare presentation to the user
+    result = cursor.fetchall()
+    print("\n\nDB: Printing all orders pending to be shipped...\n\n")
+    table_print.printTable(result, ["OrderId", "OrderDate", "CompanyName", "ContactName", "Country", "Phone"])
+    
+    return 0
 
 #6th Option. Restocks a product
 def restock(db, ID, num):
@@ -46,32 +169,10 @@ def restock(db, ID, num):
     db.commit()
 
     #Prepare presentation to the user
-    print("\n\n", cursor.rowcount, "record(s) updated")
-    print("Successfully updated Units In Stock for the product:")
+    print("\n\nDB:", cursor.rowcount, "record(s) updated")
+    print("DB: Successfully updated Units In Stock for the product:")
     
     return 0
- 
-
-
-#5th Option. Gets pending orders
-def pending(db):
-    cursor = db.cursor()
-
-    #Prepare SQL -- Get Info of order without a shipped date
-    sql = "SELECT DISTINCT OrderID, OrderDate, CompanyName, ContactName, Country, Phone" \
-     + " FROM customers NATURAL JOIN (SELECT OrderID, CustomerID, OrderDate" \
-     + " FROM orders WHERE ShippedDate IS NULL) R1";
-
-    cursor.execute(sql)
-    
-    #Prepare presentation to the user
-    result = cursor.fetchall()
-    print("\n\nPrinting all orders pending to be shipped...\n\n")
-    table_print.printTable(result, ["OrderId", "OrderDate", "CompanyName", "ContactName", "Country", "Phone"])
-    
-    return 0
-
-
 
 # This is to be used as an imported module
 if __name__ == "__main__":
